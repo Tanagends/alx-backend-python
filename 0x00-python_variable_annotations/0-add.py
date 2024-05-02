b983fa7390d0094300a5e871da6221655f5f13e9
Tanagends <tanagends@gmail.com>
2024-05-02 18:04:37 +0200

    python variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Has a function that adds two floats with type annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats"""
+    return a + b
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function sum_list which takes a list input_list of floats 
+   as argument and returns their sum as a float.
+"""
+
+
+def 
