82ba3960aa5744f3b209a405445127a8c09a12de
Tanagends <tanagends@gmail.com>
2024-05-07 02:00:40 +0200

    python async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,22 +1,15 @@
-#!/usr/bin/env  python3
-"""n async routine called wait_n that takes in 2 int arguments
-   (in this order): n and max_delay
-"""
+#!/usr/bin/env python3
+'''Task 4 answer
+'''
 import asyncio
 from typing import List
-import random
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """spawn wait_random n times with the specified max_delay
-       return the list of all the delays (float values).
-    """
-    waits = [task_wait_random(max_delay) for _ in range(n)]
-    ordered_times: List[float] = []
-    for task in asyncio.as_completed(waits):
-        time: float = await task
-        ordered_times.append(time)
-    return ordered_times
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
